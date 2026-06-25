db3443a0cb8453f85aaef160fca38bf05eaa4ad1
Tanubasist <166485540+Tanubasist@users.noreply.github.com>
2026-06-25 14:18:43 +0530

    Fix wsgi comment syntax error

--- discount_mart/wsgi.py
+++ discount_mart/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for discount_mart project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise # <--- Ye import karein
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discount_mart.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles')) # <--- Ye line add karein
+application = WhiteNoise(application)
